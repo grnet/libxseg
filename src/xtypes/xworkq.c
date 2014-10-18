04ffe384a335a3face5d092712c25abacbe5d0cb
Chrysostomos Nanakos <cnanakos@grnet.gr>
2014-10-18 12:24:46 +0300

    xworkq: Fix coding style issues

--- src/xtypes/xworkq.c
+++ src/xtypes/xworkq.c
@@ -25,9 +25,10 @@ int xworkq_init(struct xworkq *wq, struct xlock *lock, uint32_t flags)
 	wq->flags = flags;
 	xlock_release(&wq->q_lock);
 	wq->q = xtypes_malloc(sizeof(struct xq));
-	if (!wq->q)
+	if (!wq->q) {
 		return -1;
-	if (!xq_alloc_empty(wq->q, 8)){
+	}
+	if (!xq_alloc_empty(wq->q, 8)) {
 		xtypes_free(wq->q);
 		return -1;
 	}
@@ -48,18 +49,18 @@ int __xworkq_enqueue(struct xworkq *wq, struct work *w)
 	struct xq *newq;
 	xlock_acquire(&wq->q_lock);
 	r = __xq_append_tail(wq->q, (xqindex)w);
-	if (r == Noneidx){
+	if (r == Noneidx) {
 		newq = xtypes_malloc(sizeof(struct xq));
-		if (!newq){
+		if (!newq) {
 			r = Noneidx;
 			goto out;
 		}
-		if (!xq_alloc_empty(newq, wq->q->size*2)){
+		if (!xq_alloc_empty(newq, wq->q->size * 2)) {
 			xtypes_free(newq);
 			r = Noneidx;
 			goto out;
 		}
-		if (__xq_resize(wq->q, newq) == Noneidx){
+		if (__xq_resize(wq->q, newq) == Noneidx) {
 			xq_free(newq);
 			xtypes_free(newq);
 			r = Noneidx;
@@ -79,12 +80,14 @@ int xworkq_enqueue(struct xworkq *wq, void (*job_fn)(void *q, void *arg), void *
 {
 	//maybe use xobj
 	struct work *work = xtypes_malloc(sizeof(struct work));
-	if (!work)
+	if (!work) {
 		return -1;
+	}
 	work->job_fn = job_fn;
 	work->job = job;
-	if (__xworkq_enqueue(wq, work) < 0)
+	if (__xworkq_enqueue(wq, work) < 0) {
 		return -1;
+	}
 	return 0;
 }
 
@@ -92,15 +95,16 @@ void xworkq_signal(struct xworkq *wq)
 {
 	xqindex xqi;
 	struct work *w;
-	while (xq_count(wq->q)){
-		if (wq->lock && !xlock_try_lock(wq->lock))
+	while (xq_count(wq->q)) {
+		if (wq->lock && !xlock_try_lock(wq->lock)) {
 			return;
+		}
 
 		xlock_acquire(&wq->q_lock);
 		xqi = __xq_pop_head(wq->q);
 		xlock_release(&wq->q_lock);
 
-		while (xqi != Noneidx){
+		while (xqi != Noneidx) {
 			w = (struct work *)xqi;
 			w->job_fn(wq, w->job);
 			xtypes_free(w);
@@ -108,8 +112,9 @@ void xworkq_signal(struct xworkq *wq)
 			xqi = __xq_pop_head(wq->q);
 			xlock_release(&wq->q_lock);
 		}
-		if (wq->lock)
+		if (wq->lock) {
 			xlock_release(wq->lock);
+		}
 	}
 
 	return;
