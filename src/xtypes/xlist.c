506637d9879588150510a10f75dd4cd3356edd39
Chrysostomos Nanakos <cnanakos@grnet.gr>
2015-01-02 15:15:17 +0200

    xlist: Remove unmatched #endif
    Change coding style to K&R.
    
    Signed-off-by: Chrysostomos Nanakos <cnanakos@grnet.gr>

--- src/xtypes/xlist.c
+++ src/xtypes/xlist.c
@@ -19,77 +19,74 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 void __xlist_detach(struct xlist_node *node)
 {
-	struct xlist_node *head, *tail;
-	head = XPTR(&node->head);
-	tail = XPTR(&node->tail);
-	if (head) {
-		XPTRSET(&head->tail, tail);
-	}
-	if (tail) {
-		XPTRSET(&tail->head, head);
-	}
-	XPTRSET(&node->pool, NULL);
+    struct xlist_node *head, *tail;
+    head = XPTR(&node->head);
+    tail = XPTR(&node->tail);
+    if (head) {
+        XPTRSET(&head->tail, tail);
+    }
+    if (tail) {
+        XPTRSET(&tail->head, head);
+    }
+    XPTRSET(&node->pool, NULL);
 }
 
-void __xlist_attach(	struct xlist_node *head,
-			struct xlist_node *tail,
-			struct xlist_node *node	)
+void __xlist_attach(struct xlist_node *head,
+                    struct xlist_node *tail, struct xlist_node *node)
 {
-	struct xlist *list = XPTR(node->list);
-	xqindex nr = XPTRI(&list->node.list);
+    struct xlist *list = XPTR(node->list);
+    xqindex nr = XPTRI(&list->node.list);
 
-	if (!list || !nr) {
-		return;
-	}
+    if (!list || !nr) {
+        return;
+    }
 
-	XPTRSET(&node->head, head);
-	XPTRSET(&node->tail, tail);
-	XPTRSET(&head->tail, node);
-	XPTRSET(&tail->head, node);
-	XPTRISET(&list->node.list, nr - 1);
+    XPTRSET(&node->head, head);
+    XPTRSET(&node->tail, tail);
+    XPTRSET(&head->tail, node);
+    XPTRSET(&tail->head, node);
+    XPTRISET(&list->node.list, nr - 1);
 }
 
-xqindex xlist_add_head(struct xlist *list, struct xlist_node *node)
+xqindex xlist_add_head(struct xlist * list, struct xlist_node * node)
 {
-	struct xlist_node *head;
-	xqindex nr = XPTRI(&list->node.list) + 1;
+    struct xlist_node *head;
+    xqindex nr = XPTRI(&list->node.list) + 1;
 
-	if (nr == Noneidx) {
-		goto out;
-	}
+    if (nr == Noneidx) {
+        goto out;
+    }
 
-	__xlist_detach(node);
-	head = XPTR(&node->head);
-	__xlist_attach(head, &list->node, node);
+    __xlist_detach(node);
+    head = XPTR(&node->head);
+    __xlist_attach(head, &list->node, node);
 
-	XPTRISET(&list->node.list, nr);
-out:
-	return nr;
+    XPTRISET(&list->node.list, nr);
+  out:
+    return nr;
 }
 
-xqindex xlist_add_tail(struct xlist *list, struct xlist_node *node)
+xqindex xlist_add_tail(struct xlist * list, struct xlist_node * node)
 {
-	struct xlist_node *tail;
-	xqindex nr = XPTRI(&list->node.list) + 1;
+    struct xlist_node *tail;
+    xqindex nr = XPTRI(&list->node.list) + 1;
 
-	if (nr == Noneidx) {
-		goto out;
-	}
+    if (nr == Noneidx) {
+        goto out;
+    }
 
-	__xlist_detach(node);
-	tail = XPTR(&node->tail);
-	__xlist_attach(&list->node, tail, node);
+    __xlist_detach(node);
+    tail = XPTR(&node->tail);
+    __xlist_attach(&list->node, tail, node);
 
-	XPTRISET(&list->node.list, nr);
-out:
-	return nr;
+    XPTRISET(&list->node.list, nr);
+  out:
+    return nr;
 }
 
 struct xlist *xlist_detach(struct xlist_node *node)
 {
-	struct xlist *list = node->list;
-	__xlist_detach(node);
-	return list;
+    struct xlist *list = node->list;
+    __xlist_detach(node);
+    return list;
 }
-
-#endif
