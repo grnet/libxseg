41347dbc13341e0d70a3bdb86748951bb6e3e614
Chrysostomos Nanakos <cnanakos@grnet.gr>
2015-01-02 15:00:44 +0200

    initialize: Change coding style to K&R
    Signed-off-by: Chrysostomos Nanakos <cnanakos@grnet.gr>

--- src/initialize.c
+++ src/initialize.c
@@ -21,16 +21,16 @@ int xseg_posixfd_init(void);
 
 int __xseg_preinit(void)
 {
-	int r;
-	if ((r = xseg_posix_init())) {
-		goto out;
-	}
-	if ((r = xseg_pthread_init())) {
-		goto out;
-	}
-	if ((r = xseg_posixfd_init())) {
-		goto out;
-	}
-out:
-	return r;
+    int r;
+    if ((r = xseg_posix_init())) {
+        goto out;
+    }
+    if ((r = xseg_pthread_init())) {
+        goto out;
+    }
+    if ((r = xseg_posixfd_init())) {
+        goto out;
+    }
+  out:
+    return r;
 }
